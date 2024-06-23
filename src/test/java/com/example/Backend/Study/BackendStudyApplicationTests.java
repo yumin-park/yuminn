5a26ba7166bf66706dfc2986767853a64ac7f55e
yumin <rdddq01@gmail.com>
2024-06-23 18:31:30 +0900

    Signup Study

---
+++ src/test/java/com/example/Backend/Study/BackendStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Backend.Study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
